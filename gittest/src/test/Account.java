1d67212be5b8a1c15176f558f8e273c7d92f47fb
yunseo <redtastes@naver.com>
2022-03-28 16:40:51 +0900

    Account 객체 생성

---
+++ gittest/src/test/Account.java
@@ -0,0 +1,13 @@
+package test;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Account {
+ private String ano,owner;
+ private int balance;
+}
